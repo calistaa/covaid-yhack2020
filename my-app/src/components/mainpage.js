905be2b6634d485347abea0eee2a2d28d20a5733
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-11-10 10:59:51 -0800

    initial changes

---
+++ my-app/src/components/mainpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Album from 'react-bootstrap/Album'
+
+class Album extends React.Component {
+    render(){
+        return(
+            <div className="album">
+                
+            </div>
+        );
+    }
+}
