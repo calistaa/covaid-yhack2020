29ce797bd4babda29ba55e89454626d5b87df383
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-11-10 22:08:02 -0800

    New Home Page

--- front-end/src/components/HomePage/index.jsx
+++ front-end/src/components/HomePage/index.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
 
+import Button from '../Button';
+
+import './style.less';
+
+const HomePage = () => {
+    return (
+        <div className = "homepage">
+            <div className = "content">
+                <h1> Welcome To Helping Hands!</h1>
+                <h2> I am a: </h2>
+            </div>
+            <NavLink activeClassName = "selected" to = "/ibegin">
+                <Button color = '#BFCC94' size = '25px' width = '288px'>
+                    Volunteer
+                </Button>
+            </NavLink>
+
+            <NavLink activeClassName = "selected" to = "/ibegin">
+                <Button color = '#BFCC94' size = '25px' width = '288px'>
+                    Organization
+                </Button>
+            </NavLink>
+        </div>
+    )
+}
