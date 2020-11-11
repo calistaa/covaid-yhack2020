5757e23f7722de318a80d62e752175fca4e2954f
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-11-10 21:35:05 -0800

    Nav bar

--- front-end/src/components/Navbar/index.jsx
+++ front-end/src/components/Navbar/index.jsx
@@ -1,1 +1,23 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
+import './style.less';
+
+const Navbar = props => {
+    return (
+        <nav classname = "map-nav-bar">
+            <NavLink to = "/">
+                <p> about </p>
+            </NavLink>
+            <NavLink to = "/">
+                <p> organizations </p>
+            </NavLink>
+            <NavLink to = "/">
+                <p> faq </p>
+            </NavLink>
+            <div className = "modal" onclick = {props.viewBuildings}>
+                <p>Sign Up/Sign In</p>
+            </div>
+        </nav>
+    );
+}
