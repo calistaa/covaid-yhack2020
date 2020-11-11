d6a006e0cffee51812aeb3e5b559dfe207cf4582
snguyen740 <66283760+snguyen740@users.noreply.github.com>
2020-11-10 16:09:27 -0800

    new home page

--- front-end/src/components/homepage/index.jsx
+++ front-end/src/components/homepage/index.jsx
@@ -1,1 +1,35 @@
+import React from 'react';
 
+import Button from '../Button';
+
+import floating from 'my-app/public/left_man.svg';
+
+import './style.less';
+
+const homepage = () => {
+    return (
+        <div classname="homepage">
+            <div classname = "content">
+                <h1> Helping Hands </h1>
+                <div classname = "subcontent">
+                    <h2> Hello, welcome to our community! </h2>
+                    <h3> I am a: </h3>
+                </div>
+                <NavLink activeClassname = "selected" to="/ibegin">
+                    <Button color = '#BFCC94' size = '25px' width='200px'>
+                        Volunteer
+                    </Button>
+                </NavLink>
+
+                <NavLink activeClassname = "selected" to="/ibegin">
+                    <Button color = '#BFCC94' size = '25px' width='200px'>
+                        Organization
+                    </Button>
+                </NavLink>
+                <div className="floatingPerson">
+                    <img src={floating} alt="floating"/>
+                </div>
+            </div>
+        </div>
+    )
+}
